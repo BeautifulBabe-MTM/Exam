f5e8a49f607ae082819d700a654d0f44487d847d
Yaroslav <pitnichyk@gmail.com>
2021-07-08 19:04:46 -0400

    back up

--- WindowsFormsApp1/Settings.Designer.cs
+++ WindowsFormsApp1/Settings.Designer.cs
@@ -28,100 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
+            this.FolderButton = new System.Windows.Forms.Button();
+            this.BGColor = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.LangBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // FolderButton
+            // 
+            resources.ApplyResources(this.FolderButton, "FolderButton");
+            this.FolderButton.Name = "FolderButton";
+            this.FolderButton.UseVisualStyleBackColor = true;
+            this.FolderButton.Click += new System.EventHandler(this.FolderButton_Click);
+            // 
+            // BGColor
+            // 
+            resources.ApplyResources(this.BGColor, "BGColor");
+            this.BGColor.Name = "BGColor";
+            this.BGColor.UseVisualStyleBackColor = true;
+            this.BGColor.Click += new System.EventHandler(this.BGColor_Click);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label1.Location = new System.Drawing.Point(21, 21);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Язык";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.Location = new System.Drawing.Point(21, 63);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Сохранить";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label3.Location = new System.Drawing.Point(21, 103);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(88, 17);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Задний фон";
             // 
-            // button1
+            // LangBox
             // 
-            this.button1.Location = new System.Drawing.Point(115, 60);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Выбрать";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(115, 100);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Цвет";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "ru",
-            "en-US",
-            "uk",
-            "az"});
-            this.comboBox1.Location = new System.Drawing.Point(115, 20);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(75, 21);
-            this.comboBox1.TabIndex = 5;
+            resources.ApplyResources(this.LangBox, "LangBox");
+            this.LangBox.FormattingEnabled = true;
+            this.LangBox.Items.AddRange(new object[] {
+            resources.GetString("LangBox.Items"),
+            resources.GetString("LangBox.Items1"),
+            resources.GetString("LangBox.Items2")});
+            this.LangBox.Name = "LangBox";
+            this.LangBox.SelectedIndexChanged += new System.EventHandler(this.LangBox_SelectedIndexChanged);
             // 
             // Settings
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(207, 142);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LangBox);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.BGColor);
+            this.Controls.Add(this.FolderButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Settings";
-            this.Text = "Настройки";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Button FolderButton;
+        private System.Windows.Forms.Button BGColor;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox LangBox;
     }
 }
