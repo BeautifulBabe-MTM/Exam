5e97a9ebee4125fdff6b7bb7ce6644dd7b19e0ed
Yaroslav <pitnichyk@gmail.com>
2021-07-08 15:56:44 -0400

    realizing Note class

---
+++ ClassLibrary1/Manager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class Manager
+    {
+        public static List<Note> notes { get; private set; } = new List<Note>();
+        public static void CreateNote(string noteName, string text, DateTime creationData)
+        {
+            notes.Add(new Note(noteName, text, creationData));
+        }
+        public static string TakeTextNote(string notename) => notes.Find(x => x.Title == notename).Text;
+        public static void DeleteNote(string noteName)
+        {
+            notes.RemoveAt(notes.FindIndex(item => item.Title == noteName));
+        }
+        public static void SaveAllNotes(string path)
+        {
+            if (path.EndsWith(".txt"))
+                NoteFile.SaveNotes(path, notes.ToArray());
+            
+            else
+            {
+                path += ".txt";
+                NoteFile.SaveNotes(path, notes.ToArray());
+            }
+        }
+        public static void SaveNote(string noteName, string path)
+        {
+            NoteFile.SaveNote(path, notes.Find(item => item.Title == noteName));
+        }
+        public static void ReadNotesFromFile(string path)
+        {
+            notes = NoteFile.ReadFromFile(path);
+        }
+    }
+}
---
+++ ClassLibrary1/Note.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime CreationData { get; private set; }
+
+        public Note()
+        {
+            Title = string.Empty;
+            Text = string.Empty;
+            CreationData = new DateTime();
+        }
+
+        public Note(string noteName, string text, DateTime creationData)
+        {
+            Text = text;
+            Title = noteName;
+            CreationData = creationData;
+        }
+    }
+}
